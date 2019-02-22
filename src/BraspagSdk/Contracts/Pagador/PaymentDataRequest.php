cba784c4ecfc69bbfc7df120e862eced0677037d
Ricardo Abdalla <ricardoabdalla@outlook.com>
2019-02-22 11:00:49 -0300

    /30602: Auto stash before merge of "feature/30602" and "origin/feature/30602"

--- src/BraspagSdk/Contracts/Pagador/PaymentDataRequest.php
+++ src/BraspagSdk/Contracts/Pagador/PaymentDataRequest.php
@@ -72,15 +72,15 @@ class PaymentDataRequest
 
     public $ExtraDataCollection;
 
-//    public function __construct()
-//    {
-//        $this->CreditCard = new CreditCardData();
-//        $this->DebitCard = new DebitCardData();
-//        $this->Wallet = new WalletData();
-//        $this->Credentials = new CredentialsData();
-//        $this->ExternalAuthentication = new ExternalAuthenticationData();
-//        $this->FraudAnalysis = new FraudAnalysisRequestData();
-//        $this->RecurrentPayment = new RecurrentPaymentDataRequest();
-//        $this->ExtraDataCollection = new ExtraData();
-//    }
+    public function __construct()
+    {
+        $this->CreditCard = new CreditCardData();
+        $this->DebitCard = new DebitCardData();
+        $this->Wallet = new WalletData();
+        $this->Credentials = new CredentialsData();
+        $this->ExternalAuthentication = new ExternalAuthenticationData();
+        $this->FraudAnalysis = new FraudAnalysisRequestData();
+        $this->RecurrentPayment = new RecurrentPaymentDataRequest();
+        $this->ExtraDataCollection = array();
+    }
 }
